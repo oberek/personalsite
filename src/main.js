53f4175aac5a2922f3afb3bed37c885c078e1587
Maciej Smusz <maciejsmusz@gmail.com>
2018-08-18 23:33:47 -0600

    Last commit for the night

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VeeValidate from 'vee-validate';
+import VueCarousel from 'vue-carousel';
 
 Vue.use(VeeValidate);
-
+Vue.use(VueCarousel);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,33 +1,66 @@
 <template>
 
-  <div class="base">
-    <h2>Web Projects</h2>
-    <div class="box colorize">
-      <div class="left_part">
-        <img src="https://i.ytimg.com/vi/R-RbmqzRC9c/maxresdefault.jpg" class="project_image">
-      </div>
-      <div class="right_part">
-        This project is about stuff and things
-      </div>
-    </div>
-    <div class="box dogfinder">
-      <div class="left_part">
+    <div class="base">
+        <h1>Project Work</h1>
+        <div class="box colorize">
+            <div class="left_part">
+                <!--<img src="https://i.ytimg.com/vi/R-RbmqzRC9c/maxresdefault.jpg" class="project_image">-->
+                <carousel :per-page="1">
+                    <slide>
+                        <img src="https://image.ibb.co/dx9ADK/Colorize_Interface.png" class="project_image"/>
+                    </slide>
+                    <slide>
+                        <img src="https://image.ibb.co/nvjxce/Twilight_Zone209bw.png" class="project_image"/>
+                    </slide>
+                    <slide>
+                        <img src="https://image.ibb.co/esYOHe/Twilight_Zone209.png" class="project_image"/>
+                    </slide>
+                </carousel>
+            </div>
+            <div class="right_part">
+                "Colorize" by Maciej Smusz, a web development student, won the Faculty Favorite. He developed his
+                project for Capstone: A course where student's prove their ready to tackle the challenges of the tech
+                industry and Enterprise Projects. Maciej's passion for classic movies led him to train a convolutional
+                neural network to automatically add color to old film footage. He says one of the biggest challenges of
+                the project was working with new and experimental technologies with few resources to rely on.
+            </div>
+        </div>
+        <div class="box dogfinder">
+            <div class="left_part">
+                <carousel :per-page="1">
+                    <slide>
+                        <img src="https://image.ibb.co/jjJsSe/Dog_Finder_Splash.png" class="project_image"/>
+                    </slide>
+                    <slide>
+                        <img src="https://image.ibb.co/m6780z/Dog_Finder1.png" class="project_image"/>
+                    </slide>
+                </carousel>
+            </div>
+            <div class="right_part">
+                DogFinder is a Tinder-like application which utilizes AngularJS and consumes the PetFinder API to allow
+                users to "swipe left" or "swipe right" on dogs in their area. The user can specify between young, adult,
+                and senior dogs, as well as the area they want to look in.
+            </div>
+        </div>
+        <div class="box aaft2">
+            <div class="left_part">
+                <carousel :per-page="1">
+                    <slide>
+                        <img src="https://image.ibb.co/iwUfxe/AAFT2_Splash.png" class="project_image"/>
+                    </slide>
+                    <slide>
+                        <img src="https://image.ibb.co/dYH0xe/AAFT2_Gameplay.png" class="project_image"/>
+                    </slide>
+                </carousel>
+            </div>
+            <div class="right_part">
+                Adolescent Atomic Fighting Tortoises 2 was a game created for the Web Game Development course. The game
+                utilizes CreateJS and uses the human voice as its primary mechanic. Screaming into the microphone
+                releases bones, which in turn damage your enemies, the tortoises.
+            </div>
+        </div>
 
-      </div>
-      <div class="right_part">
-        This project is about stuff and things
-      </div>
     </div>
-    <div class="box reggorf">
-      <div class="left_part">
-
-      </div>
-      <div class="right_part">
-        This project is about stuff and things
-      </div>
-    </div>
-
-  </div>
 </template>
 
 <script>
@@ -37,31 +70,56 @@
 </script>
 
 <style scoped>
-  .base {
-    margin-top: 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  }
-  h2{
-    margin-top: 0;
-  }
-  .box{
+    .base {
+        margin-top: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
 
-    display: flex;
-    min-height: 80px;
-    /*min-width: 900px;*/
-    background-color: lightgray;
-    /*transform: rotate3d(-2, 1, 1, 15deg);*/
-    margin: 20px;
-    flex-direction: row;
-  }
-  .project_image{
-    max-height: 200px;
-    padding: 10px;
-    /*box-shadow: 1px 1px 1px  ;*/
-    filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.5));
-    border-radius: 18px;
-  }
+    h2 {
+        margin-top: 0;
+    }
+
+    .box {
+        display: flex;
+        min-height: 80px;
+        /*min-width: 900px;*/
+        background-color: rgba(153, 159, 162, 0.7);
+        /*transform: rotate3d(-2, 1, 1, 15deg);*/
+        margin: 20px;
+        flex-direction: row;
+        max-width: 55%;
+        border-radius: 7px;
+    }
+
+    .project_image {
+        max-height: 300px;
+        padding: 10px;
+        /*box-shadow: 1px 1px 1px  ;*/
+        filter: drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.5));
+        border-radius: 18px;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .left_part {
+        min-width: 500px;
+        padding: 20px;
+    }
+
+    .right_part {
+        padding: 20px;
+        padding-top: 30px;
+        font-size: 20px;
+        text-shadow: 1px 1px 20px #4d4d4d;
+
+    }
+
+    .VueCarousel-slide {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
